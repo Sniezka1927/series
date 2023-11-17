9558c332b52e5c08b55520dbae89be9dbb459b17
a.dygon <dygons14@gmail.com>
2023-11-17 19:42:09 +0100

    Verified accuracy and max_steps limit

--- Projekt01/Projekt01.c
+++ Projekt01/Projekt01.c
@@ -19,12 +19,10 @@ double fx(double x) {
 double series_sum(double x, int max_steps, double accuracy, int* code, int* counter)
 {
     double sum = 1.;
-    long double nominator = 5.0;
-    long double denominator = 2.0;
+    double nominator = 5.0;
+    double denominator = 2.0;
 
-    
     for (int i = 1; i < max_steps; i++) {
-        *counter += 1;
         
         double quotient = nominator / denominator;      
         double single_step = quotient * pow(x, i);
@@ -35,39 +33,36 @@ double series_sum(double x, int max_steps, double accuracy, int* code, int* coun
         }
 
         sum += single_step;
+        *counter += 1;
 
         nominator *= (2. * i + 5.);
         denominator *= (2. * i + 2.);
-
     }
-    
+   
     return sum;
 }
 
-
 int main()
 {
-    // double a, b;
-    double a = -0.9;
-    double b = 0.9;
-    int total_divisions = 250;
+    double a, b;
+    int total_divisions;
 
     double max_domain = 1.;
-    double accuracy = 8.854 * 1e-12;
-    //double accuracy = 1e-3;
-    // max steps to get real value is 149
-    // if max_steps is over 150 it overflows in intermediate operations and returns infinity / -infinity | -nan depending on x value in quotient
-    int max_steps = 155;
-   
-
-    printf("Program that calculates series and function value in [a,b] slice, while domain is |x| < 1 \n");
-
+    // It is the perfect accuracy to optimalize overflowing and provide the most accurate values between [-.902,.902]
+    double accuracy = 0.0003;
     
+    // max steps to get real value is 150
+    // if max_steps is over 150 it overflows in intermediate operations and returns infinity / -infinity | -nan depending on x value in step
+    int max_steps = 150;
+   
+    printf("Program that calculates series and function value in [a,b] slice, while domain is |x| < 1 \n");
+ 
     printf("Provide the starting value:");
     if (scanf("%lf", &a) != 1) {
         printf("Provided incorrect value");
         return 1;
     }
+
     if (fabs(a) >= max_domain) {
         printf("Provided value does not fit into domain |x| < 1.");
         return 1;
@@ -78,15 +73,18 @@ int main()
         printf("Provided incorrect value");
         return 1;
     }  
+
     if (fabs(b) >= max_domain || b <= a) {
         printf("Providing value does not fit int domiain |x| < 1 or b is not higher than a");
         return 1;
     }
-    printf("Provide total divions: ");
+
+    printf("Provide total divions (integer): ");
     if (scanf("%d", &total_divisions) != 1) {
         printf("Provided incorrect value");
         return 1;
     }
+
     if (total_divisions <= 0) {
         printf("You have to split the range for atleast 1 piece");
         return 1;
@@ -95,15 +93,15 @@ int main()
 
     double spacing = (b - a) / total_divisions;
 
-    //  FILE *file;
-    //  file = fopen("./result.txt", "w+");
-    //  if (file == NULL) {
-    //        printf("We could not find the results file or it could not be opened. Please try again.\n");
-    //        return 1;
-    //}
+    FILE *file;
+    file = fopen("./result.txt", "w+");
+    if (file == NULL) {
+          printf("We could not find the results file or it could not be opened. Please try again.\n");
+          return 1;
+    }
 
-    // fprintf(file, "%-20s %-20s %-20s %-20s %-20s\n", "x", "fx", "series", "counter", "exit reason");
-    printf("%-20s %-20s %-20s %-20s %-20s\n", "x", "fx", "series", "counter", "exit reason");
+    fprintf(file, "%-20s %-20s %-20s %-20s %-20s\n", "x", "series", "fx", "series counter", "exit reason");
+    //printf("%-20s %-20s %-20s %-20s %-20s\n", "x",  "series","fx", "counter", "exit reason");
 
     for (int i = 0; i <= total_divisions; i++) {
         double current = a + (spacing * i);
@@ -112,18 +110,18 @@ int main()
         double fx_v = fx(current);
         double estimated = series_sum(current, max_steps, accuracy, &code, &counter);
         if (code == 0) {
-            // fprintf(file, "%-20.8lf %-20.8lf %-20.8lf %-20d %-15s\n", current, fx_v, estimated, counter, "steps limit reached");
-            printf("%-20.8lf %-20.8lf %-20.8lf %-8d steps limit reached\n", current, fx_v, estimated, counter);
-
+            //printf("%-20.8lf %-20.8lf %-20.8lf %-8d steps limit reached\n", current, estimated, fx_v, counter);
+            fprintf(file, "%-20.8lf %-20.8lf %-20.8lf %-20d %-15s\n", current, estimated, fx_v, counter, "steps limit reached");
         }
         else {
-            // fprintf(file, "%-20.8lf %-20.8lf %-20.8lf %-20d %-15s\n", current, fx_v, estimated, counter, "accuracy too small");
-            printf("%-20.8lf %-20.8lf %-20.8lf %-8d accuracy too small\n", current, fx_v, estimated, counter);
-
+            //printf("%-20.8lf %-20.8lf %-20.8lf %-8d accuracy too small\n", current, estimated, fx_v,  counter);
+            fprintf(file, "%-20.8lf %-20.8lf %-20.8lf %-20d %-15s\n", current, estimated, fx_v,  counter, "accuracy too small");
+            
         }
-        // fflush(file); // Flush the buffer to write immediately
+        // Flush the buffer to write immediately
+        // fflush(file); 
     }
-    // fclose(file);
+    fclose(file);
     printf("Results are saved to results.txt file");
     return 0;
 }
